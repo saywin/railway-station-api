fc029256ba97a7d88f485573f6eacef746d8c0eb
Artem <stilon670@gmail.com>
2025-03-14 12:38:53 +0200

    Added path api/v1/user/

--- conf/urls.py
+++ conf/urls.py
@@ -23,5 +23,6 @@ from conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/station", include("station.urls", namespace="station")),
+    path("api/v1/station/", include("station.urls", namespace="station")),
+    path("api/v1/user/", include("user.urls", namespace="user")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
