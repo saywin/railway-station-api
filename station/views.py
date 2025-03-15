b0648cc2464e2a66e40afcd41fdb1af4da7a3662
Artem <stilon670@gmail.com>
2025-03-15 15:25:39 +0200

    Added viewsets for TrainType, Train, Crew, Station, Route

--- station/views.py
+++ station/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from station.models import (
+    TrainTypeModel,
+    TrainModel,
+    CrewModel,
+    StationModel,
+    RouteModel,
+    JourneyModel,
+    OrderModel,
+    TicketModel,
+)
+from station.serializers import (
+    TrainTypeSerializer,
+    TrainListSerializer,
+    TrainSerializer,
+    TrainDetailSerializer,
+    CrewSerializer,
+    StationSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    JourneySerializer,
+    JourneyListSerializer,
+    JourneyDetailSerializer,
+    OrderSerializer,
+    TicketSerializer,
+)
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainTypeModel.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = TrainModel.objects.select_related()
+    serializer_class = TrainListSerializer
+
+    def get_serializer_class(self):
+        print(self.action)
+        if self.action == "list":
+            return TrainListSerializer
+        if self.action == "retrieve":
+            return TrainDetailSerializer
+        return self.serializer_class
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = CrewModel.objects.all()
+    serializer_class = CrewSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = StationModel.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = RouteModel.objects.all()
+    serializer_class = RouteSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ["list", "retrieve"]:
+            queryset = queryset.select_related("source", "destination")
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return RouteSerializer
+
 
 # Create your views here.
