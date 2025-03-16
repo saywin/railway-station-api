0a392f3ad1153b2b9830dcbe71bd6c8b0b17e1bd
Artem <stilon670@gmail.com>
2025-03-16 15:34:07 +0200

    Add LoginUserView and redefined TokenRefreshView, TokenVerifyView to user/views to add extend_schema

--- user/urls.py
+++ user/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+
+from user.views import (
+    ManageUserView,
+    UserCreateView,
+    LoginUserView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
-from user.views import ManageUserView, UserCreateView
-
-
 urlpatterns = [
     path("me/", ManageUserView.as_view(), name="manage_user"),
     path("user-create/", UserCreateView.as_view(), name="user_create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", LoginUserView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,17 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.settings import api_settings
+from rest_framework_simplejwt.views import (
+    TokenRefreshView as DjangoTokenRefreshView,
+    TokenVerifyView as DjangoTokenVerifyView,
+)
+
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
-from user.serializers import UserSerializer
-
-
+@extend_schema(tags=["User API"])
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
@@ -13,6 +20,23 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
         return self.request.user
 
 
+@extend_schema(tags=["User API"])
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = ()
+
+
+@extend_schema(tags=["Authenticated"])
+class LoginUserView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
+
+
+@extend_schema(tags=["Authenticated API"])
+class TokenRefreshView(DjangoTokenRefreshView):
+    pass
+
+
+@extend_schema(tags=["Authenticated API"])
+class TokenVerifyView(DjangoTokenVerifyView):
+    pass
