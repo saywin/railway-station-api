1b395dc3d8c623b124d599fde569698bdc966f06
Artem <stilon670@gmail.com>
2025-03-16 10:30:27 +0200

    Added throttle to settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -149,4 +149,9 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "50/hour", "user": "150/hour"},
 }
