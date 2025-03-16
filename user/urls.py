1da850163e5ade7f4e506ab6a4e38cbfc0a4cc90
Artem <stilon670@gmail.com>
2025-03-16 14:14:44 +0200

    Added paths me and user-create

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
-urlpatterns = []
+from user.views import ManageUserView, UserCreateView
+
+
+urlpatterns = [
+    path("me/", ManageUserView.as_view(), name="manage_user"),
+    path("user-create/", UserCreateView.as_view(), name="user_create"),
+]
 
 app_name = "user"
