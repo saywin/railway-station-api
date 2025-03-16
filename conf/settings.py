06b8b1bd68d7d359b2e7ae6859ed4929552210e7
Artem <stilon670@gmail.com>
2025-03-16 15:05:43 +0200

    Add path token, refresh, verify

--- conf/settings.py
+++ conf/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework_simplejwt",
     "station",
     "user",
     "drf_spectacular",
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from user.views import ManageUserView, UserCreateView
 
@@ -6,6 +11,9 @@ from user.views import ManageUserView, UserCreateView
 urlpatterns = [
     path("me/", ManageUserView.as_view(), name="manage_user"),
     path("user-create/", UserCreateView.as_view(), name="user_create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 app_name = "user"
