40d261f9765a921b955951f59d46641884c6db63
Artem <stilon670@gmail.com>
2025-03-16 10:15:55 +0200

    Added global pagination size 5

--- conf/settings.py
+++ conf/settings.py
@@ -144,3 +144,9 @@ AUTH_USER_MODEL = "user.User"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+}
