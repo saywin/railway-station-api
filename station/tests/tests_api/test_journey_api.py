6bbbfd25d7f9c1f457ea82e3fce1c36cdb47d15b
Artem <stilon670@gmail.com>
2025-03-18 18:09:17 +0200

    Change tests for journey_api with tickets_available

--- station/tests/tests_api/test_journey_api.py
+++ station/tests/tests_api/test_journey_api.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from django.db.models import F, Count
 from pytz import timezone
 
 from django.contrib.auth import get_user_model
@@ -25,6 +27,13 @@ ROUTE = create_route()
 TRAIN = create_train()
 
 
+def route_with_annotate():
+    return JourneyModel.objects.annotate(
+        tickets_available=F("train__cargo_num") * F("train__places_in_cargo")
+        - Count("tickets")
+    )
+
+
 def detail_route_url(pk: int) -> str:
     return reverse("station:journey-detail", args=[pk])
 
@@ -49,16 +58,23 @@ class AuthorizedJourneyTest(APITestCase):
         self.journey = create_journey()
         self.journey_2 = create_journey(route=route_2)
         self.journey_3 = create_journey(route=route_3, departure_time=departure_time)
-        self.serializer_1 = JourneyListSerializer(self.journey)
-        self.serializer_2 = JourneyListSerializer(self.journey_2)
-        self.serializer_3 = JourneyListSerializer(self.journey_3)
+        journeys = route_with_annotate().filter(
+            id__in=[self.journey.id, self.journey_2.id, self.journey_3.id]
+        )
+
+        serialized_data = JourneyListSerializer(journeys, many=True).data
+
+        self.serializer_1 = serialized_data[0]
+        self.serializer_2 = serialized_data[1]
+        self.serializer_3 = serialized_data[2]
 
         self.client.force_authenticate(user)
 
     def test_journey_list(self):
         res = self.client.get(URL_JOURNEY_LIST)
-        routes = JourneyModel.objects.all()
+        routes = route_with_annotate()
         serializer = JourneyListSerializer(routes, many=True)
+        print(serializer.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(routes.count(), 3)
         self.assertIn(serializer.data[0], res.data["results"])
@@ -70,30 +86,28 @@ class AuthorizedJourneyTest(APITestCase):
         serializer = JourneyDetailSerializer(self.journey)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        print(res.data)
-        print(JourneyDetailSerializer(self.journey).data)
         self.assertEqual(res.data, serializer.data)
 
     def test_journey_filter_by_source(self):
         res_filter_from = self.client.get(URL_JOURNEY_LIST, {"from": "dni"})
         self.assertEqual(res_filter_from.status_code, status.HTTP_200_OK)
-        self.assertIn(self.serializer_1.data, res_filter_from.data["results"])
-        self.assertIn(self.serializer_2.data, res_filter_from.data["results"])
-        self.assertNotIn(self.serializer_3.data, res_filter_from.data["results"])
+        self.assertIn(self.serializer_1, res_filter_from.data["results"])
+        self.assertIn(self.serializer_2, res_filter_from.data["results"])
+        self.assertNotIn(self.serializer_3, res_filter_from.data["results"])
 
     def test_journey_filter_by_destination(self):
         res_filter_to = self.client.get(URL_JOURNEY_LIST, {"to": "Ky"})
         self.assertEqual(res_filter_to.status_code, status.HTTP_200_OK)
-        self.assertIn(self.serializer_1.data, res_filter_to.data["results"])
-        self.assertIn(self.serializer_3.data, res_filter_to.data["results"])
-        self.assertNotIn(self.serializer_2.data, res_filter_to.data["results"])
+        self.assertIn(self.serializer_1, res_filter_to.data["results"])
+        self.assertIn(self.serializer_3, res_filter_to.data["results"])
+        self.assertNotIn(self.serializer_2, res_filter_to.data["results"])
 
     def test_journey_filter_by_date(self):
         res_filter_date = self.client.get(URL_JOURNEY_LIST, {"date": "2022-04-14"})
         self.assertEqual(res_filter_date.status_code, status.HTTP_200_OK)
-        self.assertNotIn(self.serializer_1.data, res_filter_date.data["results"])
-        self.assertNotIn(self.serializer_2.data, res_filter_date.data["results"])
-        self.assertIn(self.serializer_3.data, res_filter_date.data["results"])
+        self.assertNotIn(self.serializer_1, res_filter_date.data["results"])
+        self.assertNotIn(self.serializer_2, res_filter_date.data["results"])
+        self.assertIn(self.serializer_3, res_filter_date.data["results"])
 
     def test_journey_create_forbidden(self):
         departure_time = datetime(2023, 6, 14, 15, 34, tzinfo=timezone("Europe/Kiev"))
@@ -137,8 +151,6 @@ class AdminJourneyTest(APITestCase):
         }
 
         res = self.client.post(URL_JOURNEY_LIST, payload)
-        print(payload["route"])
-        print(res.json())
         journey = JourneyModel.objects.get(pk=res.data["id"])
         serializer = JourneySerializer(journey)
 
